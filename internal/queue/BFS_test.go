aa48b32fc9a4a6ed3e5ef252e7b0d11a8ede2194
mussabaheen <mussabaheen@gmail.com>
2022-10-29 18:59:50 +0500

    test: add test for 14 levels

--- internal/queue/BFS_test.go
+++ internal/queue/BFS_test.go
@@ -68,3 +68,36 @@ func TestMazeSolverBFS7Levels(t *testing.T) {
 	expected := []string{"[left-> forward-> upstairs-> forward-> maindoor-> downstairs->]"}
 	assert.ElementsMatch(t, expected, res)
 }
+func TestMazeSolverBFS14Levels(t *testing.T) {
+	maze := map[string]interface{}{
+		"forward": "tiger",
+		"left": map[string]interface{}{
+			"forward": map[string]interface{}{
+				"left": "tiger",
+				"upstairs": map[string]interface{}{
+					"right": map[string]interface{}{
+						"forward": "dead end",
+					},
+					"forward": map[string]interface{}{
+						"maindoor": map[string]interface{}{
+							"downstairs": map[string]interface{}{"maindoor": map[string]interface{}{"downstairs": "exit"}}},
+					},
+				},
+			},
+			"right": map[string]interface{}{
+				"forward": "dead end",
+			},
+			"upstairs": map[string]interface{}{
+				"right": map[string]interface{}{
+					"forward": "dead end",
+				},
+				"forward": map[string]interface{}{"maindoor": map[string]interface{}{
+					"downstairs": "exit",
+				}},
+			},
+		},
+	}
+	res := MazeSolverBFS(maze)
+	expected := []string{"[left-> upstairs-> forward-> maindoor-> downstairs->]", "[left-> forward-> upstairs-> forward-> maindoor-> downstairs-> maindoor-> downstairs->]"}
+	assert.ElementsMatch(t, expected, res)
+}

2d3291be4023b3ebf9165207e168a4d35f5790d9
mussabaheen <mussabaheen@gmail.com>
2022-10-29 18:53:33 +0500

    test: add test for 5 levels

--- internal/queue/BFS_test.go
+++ internal/queue/BFS_test.go
@@ -22,3 +22,25 @@ func TestMazeSolverBFS(t *testing.T) {
 	expected := []string{"[left-> left->]", "[left-> forward-> upstairs->]"}
 	assert.ElementsMatch(t, expected, res)
 }
+func TestMazeSolverBFS5Levels(t *testing.T) {
+	maze := map[string]interface{}{
+		"forward": "tiger",
+		"left": map[string]interface{}{
+			"forward": map[string]interface{}{
+				"left": "tiger",
+				"upstairs": map[string]interface{}{
+					"right": map[string]interface{}{
+						"forward": "dead end",
+					},
+					"forward": map[string]interface{}{"left": "exit"},
+				},
+			},
+			"left": "tiger",
+		},
+		"right": map[string]interface{}{
+			"forward": "dead end",
+		}}
+	res := MazeSolverBFS(maze)
+	expected := []string{"[left-> forward-> upstairs-> forward-> left->]"}
+	assert.ElementsMatch(t, expected, res)
+}
